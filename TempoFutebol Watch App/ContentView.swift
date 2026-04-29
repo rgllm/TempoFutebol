ae96d29ec7b732b45f98f17ca599d11f6f4268af
Rogério Moreira <rogerio@flecto.io>
2026-04-29 11:32:24 +0100

    Initial Commit

---
+++ TempoFutebol Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TempoFutebol Watch App
+//
+//  Created by Rogério Moreira on 29/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TempoFutebol Watch App/TempoFutebolApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempoFutebolApp.swift
+//  TempoFutebol Watch App
+//
+//  Created by Rogério Moreira on 29/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct TempoFutebol_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
